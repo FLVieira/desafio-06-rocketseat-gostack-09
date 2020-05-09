b127d72be18322dcdcae51f45277215362cb3763
Fábio Vieira <fabioluizvieiraf@gmail.com>
2020-05-09 15:36:23 -0300

    Styled-Components

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Text } from 'react-native';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function Main() {
-  return <Text>Main</Text>;
+  return <Container />;
 }
