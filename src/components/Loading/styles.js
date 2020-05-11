08203c8cee4e3f5771c8bc194a50e15c71011795
Fábio Vieira <fabioluizvieiraf@gmail.com>
2020-05-11 03:11:04 -0300

    Listando Favoritos

---
+++ src/components/Loading/styles.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components/native';
+
+export const LoadingIndicator = styled.View`
+  z-index: 1;
+  padding-top: 30px;
+`;
--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,29 +1,70 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { View } from 'react-native';
 
 import api from '../../services/api';
+import Loading from '../../components/Loading';
 
-// import { Container } from './styles';
+import {
+  Container,
+  Header,
+  Avatar,
+  Name,
+  Bio,
+  Stars,
+  Starred,
+  OwnerAvatar,
+  Info,
+  Title,
+  Author,
+} from './styles';
 
 export default class User extends Component {
   // eslint-disable-next-line react/state-in-constructor
   state = {
     stars: [],
+    loading: false,
   };
 
   async componentDidMount() {
     const { route } = this.props;
     const { user } = route.params;
 
+    this.setState({ loading: true });
     const response = await api.get(`/users/${user.login}/starred`);
 
-    this.setState({ stars: response.data });
+    this.setState({ stars: response.data, loading: false });
   }
 
   render() {
-    const { stars } = this.state;
-    return <View />;
+    const { stars, loading } = this.state;
+    const { route } = this.props;
+    const { user } = route.params;
+
+    return (
+      <Container>
+        <Header>
+          <Avatar source={{ uri: user.avatar }} />
+          <Name>{user.name}</Name>
+          <Bio>{user.bio}</Bio>
+        </Header>
+
+        <Loading loading={loading} />
+
+        <Stars
+          data={stars}
+          keyExtractor={(start) => String(start.id)}
+          renderItem={({ item }) => (
+            <Starred>
+              <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
+              <Info>
+                <Title>{item.name}</Title>
+                <Author>{item.owner.login}</Author>
+              </Info>
+            </Starred>
+          )}
+        />
+      </Container>
+    );
   }
 }
 
@@ -32,6 +73,9 @@ User.propTypes = {
     params: PropTypes.shape({
       user: PropTypes.shape({
         login: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        bio: PropTypes.string.isRequired,
+        avatar: PropTypes.string.isRequired,
       }),
     }),
   }).isRequired,
